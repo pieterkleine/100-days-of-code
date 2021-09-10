f9f888fec8d95abf6f58bb973546857d525ced71
Pieter Kleine <83700860+pieterkleine@users.noreply.github.com>
2021-09-10 17:22:04 +0200

    Add files via upload

---
+++ day-3-adventure-game.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+direction = input("You enter a forest. After a walk, the path splits. Do you go left or right?\n")
+if direction.lower() == "left":
+  swim = input("Wise choice. Next you come to a river. There is a sudden torrent. Do you swim or wait?\n")
+  if swim.lower() =="wait":
+    doorcolor = input("By waiting out until the river eases, you survive and move on. You encounter three doors. Which door do you choose to enter? Red, yellow or blue?\n")
+    if doorcolor.lower() == "red":
+      print("Burned by fire. Game over!")
+    elif doorcolor.lower() == "blue":
+      print("You are eaten by beasts. Game over!")
+    elif doorcolor.lower() =="yellow":
+      print("You found the treasure. You win!")
+    else:
+      print("Game over.")
+  else:
+    print("You are attacked by trout. Game over.")
+else:
+  print("You fall into a deep hole. Game Over.")
